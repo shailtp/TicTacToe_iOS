5644f93f463eef71a81d88bbe5a9af74302a9a1d
Shail Patel <patelshail233@gmail.com>
2023-12-05 16:12:13 -0800

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Shail Patel on 12/5/23.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
