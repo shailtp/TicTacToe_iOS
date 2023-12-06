b8528f92df9471f2fd882eabca3af7f3300dc64e
Shail Patel <patelshail233@gmail.com>
2023-12-05 16:58:37 -0800

    Intitial Commit

--- TicTacToe/ContentView.swift
+++ TicTacToe/ContentView.swift
@@ -1,24 +1,75 @@
-//
-//  ContentView.swift
-//  TicTacToe
-//
-//  Created by Shail Patel on 12/5/23.
-//
-
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+struct ContentView: View
+{
+    @StateObject var gameState = GameState()
+    
+    var body: some View
+    {
+        let borderSize = CGFloat(5)
+        
+        Text(gameState.turnText())
+            .font(.title)
+            .bold()
+            .padding()
+        Spacer()
+        
+        Text(String(format: "Crosses: %d", gameState.crossesScore))
+            .font(.title)
+            .bold()
+            .padding()
+        
+        VStack(spacing: borderSize)
+        {
+            ForEach(0...2, id: \.self)
+            {
+                row in
+                HStack(spacing: borderSize)
+                {
+                    ForEach(0...2, id: \.self)
+                    {
+                        column in
+                        
+                        let cell = gameState.board[row][column]
+                        
+                        Text(cell.displayTile())
+                            .font(.system(size: 60))
+                            .foregroundColor(cell.tileColor())
+                            .bold()
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                            .aspectRatio(1, contentMode: .fit)
+                            .background(Color.white)
+                            .onTapGesture {
+                                gameState.placeTile(row, column)
+                            }
+                    }
+                }
+                
+            }
         }
+        .background(Color.black)
         .padding()
+        .alert(isPresented: $gameState.showAlert)
+        {
+            Alert(
+                title: Text(gameState.alertMessage),
+                dismissButton: .default(Text("Okay"))
+                {
+                    gameState.resetBoard()
+                }
+            )
+        }
+        
+        Text(String(format: "Noughts: %d", gameState.noughtsScore))
+            .font(.title)
+            .bold()
+            .padding()
+        Spacer()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
